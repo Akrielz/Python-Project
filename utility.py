a7979559077ee6b77fb2d2ef406f60f18816f364
Alexandru Știrbu <34741250+Akrielz@users.noreply.github.com>
2020-12-09 01:37:48 +0200

    Add files via upload

--- utility.py
+++ utility.py
@@ -5,6 +5,16 @@ import struct
 
 
 def is_in_dir(path, argument, argument_type="file", explanation_file=("<file_target>", "file")):
+    """
+    Checks if a file or directory is in the given path
+
+    :param path: where the file / directory is located on disk
+    :param argument: the target file / directory
+    :param argument_type: specifies the type of the file(it can be "dir", "file", "both")
+    :param explanation_file: a tuple that wil be used for the error message.
+
+    :return: (found, file_path) - if it has been found or not, and its location
+    """
     target = argument
 
     if argument_type == "file":
@@ -34,10 +44,26 @@ def is_in_dir(path, argument, argument_type="file", explanation_file=("<file_tar
 
 
 def to_convention(folder_name):
+    """
+    Converts a file/folder name by making it lower and replacing each '_' with ' '
+
+    :param folder_name: the name over which to apply the convention
+
+    :return: the converted form of the name
+    """
+
     return folder_name.lower().replace("_", " ")
 
 
 def get_folder_content(path):
+    """
+    Get the names of all files and directories from the given path
+
+    :param path: where the content is located on disk
+
+    :return: (files, directories) - a tuple with an array of files and an array of directories
+    """
+
     files = []
     directories = []
     for (dir_path, dir_names, file_names) in walk(path):
@@ -49,6 +75,15 @@ def get_folder_content(path):
 
 
 def ls(path, arguments):
+    """
+    This command will print all the files and directories from the given path
+
+    :param path: the current path
+    :param arguments: a list with no elements
+
+    :return: nothing
+    """
+
     if len(arguments) != 0:
         print('Invalid Syntax! "ls" does not receive arguments\n')
         return
@@ -67,6 +102,15 @@ def ls(path, arguments):
 
 
 def cd(path, arguments):
+    """
+    This command changes the current path hto the given target directory
+
+    :param path: the current path
+    :param arguments: a list with only one element, the target directory's name
+
+    :return: either the current directory if there was an error, or the target directory
+    """
+
     if len(arguments) != 1:
         print('Invalid Syntax! "cd" receives only an argument: "cd <dir_target>"\n')
         return path
@@ -84,6 +128,19 @@ def cd(path, arguments):
 
 
 def create_archive(path, arguments):
+    """
+    This command creates an archive with all the files given as arguments
+
+    :param path: the current path
+    :param arguments: a list containing the following:
+    [archive_name, file[0] | dir[0]], file[1] | dir[1], ...]
+    Where
+    archive_name is the name of the archive (without extension);
+    file[i] | dir[i] is the name of the chosen file / directory;
+
+    :return: nothing
+    """
+
     # create_archive <archive_name> [file[0] | dir[0], file[1] | dir[1] ...]
     if len(arguments) < 2:
         print('Invalid Syntax! "create_archive" needs at least 2 arguments: ')
@@ -164,6 +221,15 @@ def create_archive(path, arguments):
 
 
 def list_content(path, arguments):
+    """
+    This command lists the content of an archive given as argument
+
+    :param path: the current path
+    :param arguments: a list containing a single argument specifying the archive's name (without extension)
+
+    :return: nothing
+    """
+
     if len(arguments) != 1:
         print('Invalid Syntax! "list_content" receives only one argument:')
         print('list_content <archive_dir>\n')
@@ -204,9 +270,18 @@ def list_content(path, arguments):
 
 
 def full_unpack(path, arguments):
+    """
+    This command unpacks every file from an archive given as argument
+
+    :param path: the current path
+    :param arguments: a list containing a single argument specifying the archive's name (without extension)
+
+    :return: nothing
+    """
+
     if len(arguments) != 1:
         print('Invalid Syntax! "full_unpack" receives only one argument:')
-        print('full_unpack <archive_dir>\n')
+        print('full_unpack <archive_name>\n')
         return
 
     target_zip = arguments[0] + ".aky_zip"
@@ -249,9 +324,21 @@ def full_unpack(path, arguments):
 
 
 def unpack(path, arguments):
+    """
+    This command unpacks only the demanded files from an archive given as arguments
+
+    :param path: the current path
+    :param arguments: a list containing the following:
+    [archive_name, file[0], file[1], ...]
+    Where:
+    archive_name is the archive's name (without extension);
+    file[i] is the file's name from inside the content
+
+    :return: nothing
+    """
     if len(arguments) < 2:
         print('Invalid Syntax! "unpack" receives at least two arguments:')
-        print('unpack <archive_dir> <file_name>\n')
+        print('unpack <archive_name> <file_name[0]> <file_name[1]> ...\n')
         return
 
     target_zip = arguments[0] + ".aky_zip"
